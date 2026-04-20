30a9078ae2b049bd199a78588a0af82bb7f1e7eb
Ronit <roniitroyy@gmail.com>
2026-04-20 14:54:57 +0530

    Project Structure

---
+++ help.py
@@ -0,0 +1,9 @@
+logger.info("Process started")
+
+logger.warning("Missing values found")
+
+logger.error("Model training failed")
+
+logger.debug("Feature engineering step")
+
+logger.critical("Database connection failed")
