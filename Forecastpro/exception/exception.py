ad0131df752fbd78686a5e51d8a84f46596bd4a1
Ronit <roniitroyy@gmail.com>
2026-04-20 16:17:43 +0530

    BasicConfig file and Custom Exception file is created

---
+++ Forecastpro/exception/exception.py
@@ -0,0 +1,24 @@
+import sys
+from Forecastpro.logging import logging 
+
+class ForecastProException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        self.error_message=error_message
+        _,_,exc_tb=error_details.exc_info()
+        
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename
+        
+        def __str__(self):
+            return f"Error occured in script: {self.file_name} at line number: {self.lineno} with error message: {str(self.error_message)}"
+        
+        
+# to check the exception handling is working or not we can run the below code snippet. It will raise an exception and we can check the log file for the error message.
+
+'''if __name__=="__main__":
+    
+    try:
+        logging.logging.info("Testing the exception handling")
+        a=1/0
+    except Exception as e:
+        raise ForecastProException(e,sys)'''
---
+++ Forecastpro/logging/logging.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+from time import strftime
+
+
+LOG_FILE=f"{datetime.now().strftime('%m-%d--%Y-%H-%M-%S')}.log"
+
+logs_path=os.path.join(os.getcwd(),'logs')
+
+os.makedirs(logs_path, exist_ok=True)
+
+Log_file_path=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=Log_file_path,
+    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,)
+
+
+
+
